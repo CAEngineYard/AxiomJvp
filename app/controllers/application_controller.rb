2141961880117a4e5a2718bb2672da411c2d68f0
rsubramanian <Ramanathan.TirunellaiSubramanian@careerbuilder.com>
2013-06-10 12:10:27 -0400

    6-10-2013_ver6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :force_plugin_reload
+
+  def force_plugin_reload
+    ActiveSupport::Dependencies.load_file "xml_sec.rb" if "development" == ENV
+    ActiveSupport::Dependencies.load_file "xmlcanonicalizer.rb" if "development" == ENV
+  end
+
 end
