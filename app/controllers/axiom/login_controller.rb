eee1fc6cd18eae4e879a9a954c552910f5dece1a
rsubramanian <Ramanathan.TirunellaiSubramanian@careerbuilder.com>
2013-06-10 11:52:56 -0400

    6-10-2013_ver5

--- app/controllers/axiom/login_controller.rb
+++ app/controllers/axiom/login_controller.rb
@@ -1,10 +1,10 @@
 class Axiom::LoginController < ApplicationController
   def index
-    @settings = Account.get_saml_settings()
-
-    if @settings.nil?
-      return redirect_to '/saml'
-    end
+    #@settings = Account.get_saml_settings()
+    #
+    #if @settings.nil?
+    #  return redirect_to '/saml'
+    #end
 
     # If we're viewing this unauthenticated, set our goback URL for after logging in
     if session[:userid].nil?
--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -1,9 +1,12 @@
+require 'onelogin/ruby-saml/settings'
+require 'onelogin/ruby-saml/authrequest'
+
 class SamlController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:consume]
 
   def index
     # insert identity provider discovery logic here
-    settings = Account.get_saml_settings
+    settings = get_saml_settings
     request = Onelogin::Saml::Authrequest.new
     redirect_to(request.create(settings))
   end
@@ -23,6 +26,20 @@ class SamlController < ApplicationController
     else
       redirect_to :action => :fail
     end
+  end
+
+  def get_saml_settings
+    # should retrieve SAML-settings based on subdomain, IP-address, NameID or similar
+    settings = Onelogin::Saml::Settings.new
+
+
+    settings.assertion_consumer_service_url   = 'http://corpappsdev.careerbuilder.com/saml/consume'
+    settings.issuer                           = 'http://corpappsdev.careerbuilder.com'
+    settings.idp_sso_target_url               = 'https://cbtest.okta.com/app/template_saml_2_0/k4lymtarUFNMMBSZZSOI/sso/saml'
+    settings.idp_cert_fingerprint             =  '02:05:79:D8:96:CC:CA:54:2F:16:E2:6E:7D:32:F3:54:95:61:89:71'
+    settings.name_identifier_format           = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+
+    settings
   end
 
   def complete
